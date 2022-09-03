4035b80ecd264f66c1659d81ca1eb3d3cad4a41c
castlewong <swy0114@qq.com>
2022-09-03 22:02:19 +0800

    Initial Commit

---
+++ Breathe/BreatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreatheApp.swift
+//  Breathe
+//
+//  Created by SSD4Wilbur on 2022/9/3.
+//
+
+import SwiftUI
+
+@main
+struct BreatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
